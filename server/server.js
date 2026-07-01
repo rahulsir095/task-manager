bf5a6ee95180cb36ab0af6c18b7cd6a496228b3d
Rahul Kumar <72785041+rahulsir095@users.noreply.github.com>
2026-07-01 21:01:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ import connectDB from './config/db.js';
 dotenv.config();
 import taskRouter from "./routes/task.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json())
 app.use("/task",taskRouter)
 
