de52fec276534f32a708c430dfc0708283997289
Rahul Kumar <72785041+rahulsir095@users.noreply.github.com>
2026-07-01 21:26:48 +0530

    Add bseurl to api

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.PROD 
-    ? 'https://task-manager-z7kj.onrender.com' 
-    : '/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 export default API;
